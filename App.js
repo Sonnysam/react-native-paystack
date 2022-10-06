6a2600ec3e32a28f95a92fa96a5dec2bedfb9675
sonnysam <samuelagbenyo067@gmail.com>
2022-10-06 21:22:52 +0000

    logo and payment component added

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import logo from './components/Logo';
+import Payment from './components/Payment';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <logo/>
+      <Payment/>
     </View>
   );
 }
